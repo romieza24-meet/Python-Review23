b6daf1fabc458b96e3786cc5ba8a9eba7abf01e7
romieza24-meet <139688912+romieza24-meet@users.noreply.github.com>
2023-07-17 18:05:32 +0300

    Update pythonReview.py

--- pythonReview.py
+++ pythonReview.py
@@ -5,15 +5,15 @@ def create_youtube_video(title, description, hashtag_value):
 
 
 def like(yt_video):
-
+	if "dislikes" in yt_video:
 		yt_video["likes"] += 1
 		return yt_video
 
 
 def dislike(yt_video):
-
-		yt_video["dislikes"] += 1
-		return yt_video
+		if "dislikes" in yt_video:
+			yt_video["dislikes"] += 1
+			return yt_video
 
 
 def add_comment(yt_video, username, comment_text):
