8f796632d3138bcdf92791d2b1ec2a40cd2ca9d8
romieza24-meet <139688912+romieza24-meet@users.noreply.github.com>
2023-07-17 16:32:27 +0300

    added bonus

--- pythonReview.py
+++ pythonReview.py
@@ -1,18 +1,11 @@
 
-def create_youtube_video(title, description):
-	yt_video = {"title": title, "description": description, "likes": 0, "dislikes": 0, "comments": {}}
+def create_youtube_video(title, description, hashtag_value):
+	yt_video = {"title": title, "description": description, "likes": 0, "dislikes": 0, "comments": {}, "hashtag": hashtag_value}
 	return yt_video
 
 
 def like(yt_video):
-	# for key in yt_video:
-	# 	if key == "likes":
-	# 		print(key)
-	# 		yt_video["likes"] += 1
-	# 		return yt_video
-	# 	else:
-	# 		print(key)
-	# 		return yt_video
+
 		yt_video["likes"] += 1
 		return yt_video
 
@@ -27,7 +20,28 @@ def add_comment(yt_video, username, comment_text):
 	yt_video["comments"][username] = comment_text
 	return yt_video
 
-my_video = create_youtube_video("test_title", "test_description")
+def similarity_to_video(vid1, vid2):
+	list1 = vid1["hashtag"]
+	list2 = vid2["hashtag"]
+	similarity = 0
+	similar_item = 0
+	for i in list1:
+		for v in list2:
+			if i == v:
+				similar_item += 1
+	len1 = len(list1)
+	len2 = len(list2)
+	if len1 > len2:
+		similar_item = 100*(int(similar_item)/int(len1))
+	else:
+		similar_item = 100*(int(similar_item)/int(len2))
+	return(similar_item)
+vid1 = {"likes": 0, "dislikes": 0, "comments": {}, "hashtag": ['a', 'b', 'c', 'd']}
+vid2 = {"likes": 0, "dislikes": 0, "comments": {}, "hashtag": ['a', 'b', '3']}
+print(similarity_to_video(vid1, vid2))
+
+
+my_video = create_youtube_video("test_title", "test_description", ["#test"])
 
 print(my_video)
 
